f3fd4d30993e99e25de45e7752c55c289f0296dd
bocheng.zbc <bocheng.zbc@alibaba-inc.com>
2019-03-22 21:47:01 +0800

    feat(功能新增): 完善 demo

--- demo/demo.tsx
+++ demo/demo.tsx
@@ -1,15 +1,40 @@
 import * as React from 'react';
 import { render } from 'react-dom';
-import { [CLASSNAME], I[CLASSNAME]Props } from '../src/';
+import { [CLASSNAME], [CLASSNAME]Factory, I[CLASSNAME]Props } from '../src/';
+import { Collapse } from 'antd';
+const Panel = Collapse.Panel;
+
+const { [CLASSNAME]WithStore, client } = [CLASSNAME]Factory();
 
 function onClick(value) {
   console.log('当前点击：', value);
 }
+function onClickWithStore(value) {
+  client.put(`/model`, {
+    name: 'text',
+    value: `gggg${Math.random()}`.slice(0, 8)
+  });
+}
 
 const props: I[CLASSNAME]Props = {
   visible: true
-}
+};
 
-render(<[CLASSNAME] {...props} onClick={onClick} />, document.getElementById(
-  'example'
-) as HTMLElement);
+render(
+  <Collapse defaultActiveKey={['1']}>
+    <Panel header="普通组件" key="0">
+      <[CLASSNAME] {...props} onClick={onClick} />
+    </Panel>
+    <Panel header="包含 store 功能" key="1">
+      <[CLASSNAME]WithStore onClick={onClickWithStore} />
+    </Panel>
+  </Collapse>,
+  document.getElementById('example') as HTMLElement
+);
+
+client.post('/model', {
+  model: {
+    visible: true,
+    text: `text${Math.random()}`.slice(0, 8)
+  }
+});
